f2622ec1acde5a2d4268eea2fe92bf3cf9c10327
Asustv123 <112798924+Asustv123@users.noreply.github.com>
2026-05-17 14:25:28 +0700

    Update script requirement URL for Bypass UptoLink

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://vanhcheat.rf.gd
 // @version      2.0
 // @description  Bypass UptoLink — host trên nekovcheat.io
-// @require      https://nekovcheat.io/source.js
+// @require      https://raw.githubusercontent.com/Asustv123/Bypass-uptolink1/main/source.js
 // @match        *://*/*
 // @author       NekoVCheat
 // @grant        GM_xmlhttpRequest
